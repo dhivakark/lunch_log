39e45939b347748c5db61e5fa9ed28c5a0c722e7
Hamanth Gousik <hamanthgousik@gmail.com>
2018-09-26 12:05:08 +0530

    added weekly entry with css touched html page and took holidays into account

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('pref/', views.get_preference, name='get_prefernce')
+    path('pref/', views.get_preference, name='get_prefernce'),
+    path('weekly_entry/', views.weekly_entry, name='weekly_entry'),
+    path('weekly_entry/week_updated/', views.week_updated, name='week_updated'),
 ]
--- user/views.py
+++ user/views.py
@@ -11,6 +11,19 @@ from oauth2client.client import SignedJwtAssertionCredentials
 import datetime
 import calendar
 
+def gsheet_authentication():
+	json_key = json.load(open('user/config/creds.json')) # json credentials you downloaded earlier
+	scope = ['https://spreadsheets.google.com/feeds',
+'https://www.googleapis.com/auth/drive']
+	credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope) 
+	file = gspread.authorize(credentials) # authenticate with Google
+	sheet = file.open("MUO_Python_Sheet") # open sheet
+	worksheet = sheet.sheet1
+	return worksheet
+
+
+WORKSHEET = gsheet_authentication()
+
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
@@ -28,37 +41,38 @@ def get_preference(request):
  			'pref':typ
  			})
  		if 'c' in request.POST:
- 			typ = request.POST.getlist('c')
- 			t = typ[0].split(',')
- 			print(t[0])
- 			if day == 1:
- 				return render(request, 'pref-wed.html')
+ 			if not isholiday(present_date):
+ 				if day == 2:
+ 					print(isholiday(present_date))
+ 					return render(request, 'pref-wed.html')
+ 				else:
+ 					return render(request, 'pref-other.html')
  			else:
- 				return render(request, 'pref-other.html')
- 		
+ 				return render(request, 'holiday.html')
  	return render(request, 'pref.html', {
  		'pref':'no choice'
  		})
  	
 
+def isholiday(present_date):
+	worksheet = WORKSHEET
+	count_date = 0
+	all_dates = worksheet.range('B1:P1')
+	for date in all_dates:
+		count_date += 1
+		if date.value == present_date:
+			print(worksheet.acell(str(chr(ord('A') + count_date)+'2')))
+			if worksheet.acell(str(chr(ord('A') + count_date)+'2')).value == 'h':
+				return 1
+			else:
+				return 0
 
 
-def gsheet_authentication():
-	json_key = json.load(open('user/config/creds.json')) # json credentials you downloaded earlier
-	scope = ['https://spreadsheets.google.com/feeds',
-'https://www.googleapis.com/auth/drive']
-	credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope) 
-	file = gspread.authorize(credentials) # authenticate with Google
-	sheet = file.open("MUO_Python_Sheet") # open sheet
-	worksheet = sheet.sheet1
-	return worksheet
 
 def gsheet(choice, user , present_date):
-	worksheet = gsheet_authentication()
-	choice = choice.rstrip('/')
-	user = user.rstrip('/')
+	worksheet = WORKSHEET
 	all_cells = worksheet.range('A1:A11')
-	all_dates = worksheet.range('B1:J1')
+	all_dates = worksheet.range('B1:P1')
 	count_user = 0
 	count_date = 0
 	for cell in all_cells:
@@ -69,3 +83,42 @@ def gsheet(choice, user , present_date):
 				if date.value == present_date:
 					print(chr(ord('A') + count_date)+str(count_user))
 					worksheet.update_acell(chr(ord('A') + count_date)+str(count_user), choice)
+
+def weekly_entry(request):
+	if request.method == 'POST':
+		global name
+		name = request.POST['name']
+		worksheet = WORKSHEET
+		now = datetime.datetime.now()
+		present_date = str(now)[:10]
+		day = now.weekday()
+		all_dates = worksheet.range('B1:P1')
+		global date_list
+		date_list = list()
+		count_date = 0
+		count_user = 0
+		all_cells = worksheet.range('A1:A11')
+		for cell in all_cells:
+			count_user += 1
+			if cell.value == name:
+				for date in all_dates:
+					count_date += 1
+					if date.value == present_date or 0<len(date_list)<6:
+						column = chr(ord('A') + count_date)
+						if worksheet.acell(str(column+'2')).value == 'w':
+							choice = worksheet.acell(str(column+str(count_user))).value
+							date_list.append([date.value, day%7, choice])
+						day += 1
+					if len(date_list) > 5:
+						break
+				break
+		context={'week_list':date_list}
+		return render(request, 'weekly_log.html',context)
+
+
+def week_updated(request):
+	if request.method == "POST":
+		for date in date_list:
+			choice = request.POST[date[0]]
+			gsheet(choice, name , date[0])
+	return render(request, 'pref.html')	
